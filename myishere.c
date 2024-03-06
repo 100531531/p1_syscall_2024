d64c2ba44c18b135a5ee796ac30094440b778a62
100531531 <158315509+100531531@users.noreply.github.com>
2024-03-06 14:58:31 +0100

    Working myishere.c

--- myishere.c
+++ myishere.c
@@ -5,7 +5,6 @@
 #include <dirent.h>
 #include <string.h>
 
-
 int main(int argc, char *argv[])
 {
 	/* If less than three arguments (argv[0] -> program, argv[1] -> directory to search, argv[2] -> file to find) print an error y return -1 */
@@ -15,5 +14,31 @@ int main(int argc, char *argv[])
 		return -1;
 	}
 
+	DIR *direc = opendir(argv[1]);
+
+	if (direc == NULL) {
+        printf("failed to open");
+		return -1;
+    }
+
+	// struct to represent an entry in a directory
+	struct dirent *entry;
+
+	char *fileName = argv[2];
+
+	while ((entry = readdir(direc)) != NULL) {
+
+		if (strcmp(entry->d_name, fileName) == 0) {
+			printf("File %s is in directory %s\n", fileName, argv[1]);
+            closedir(direc);
+			return 0;  // Exit the function once the file is found
+		} 
+
+	}
+
+	printf("File %s is not in directory %s\n", fileName, argv[1]);
+	closedir(direc);
 	return 0;
 }
+
+
